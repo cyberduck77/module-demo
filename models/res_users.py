801f01ad63144e8d4574a014e9cbd37dbf566870
Pham Thanh Dat (cyberduck77) <cyberduck77@odoo.com>
2023-05-25 11:47:40 +0700

    finish 16 chapters

--- models/res_users.py
+++ models/res_users.py
@@ -3,3 +3,5 @@ from odoo import models, fields
 class ResUsers(models.Model):
     _name = "res.users"
     _inherit = "res.users"
+
+    property_ids = fields.One2many('estate.property', 'user_id', domain="['|',('state','=','new'),('state','=','offer received')]")
